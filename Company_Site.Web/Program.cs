de88d417cdf2366d26e15e2b1672c9496444433f
Mehdi Avazniya <MehdiAvazniya@gmail.com>
2025-10-27 13:23:27 +0330

    Add project files.

---
+++ Company_Site.Web/Program.cs
@@ -0,0 +1,40 @@
+using Company_Site.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company_Site.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DataBaseContext>
+                (p => p.UseSqlServer("Server=DESKTOP-J95NUIR;Database=Company_Site;Trusted_Connection=True;TrustServerCertificate=True;"));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
