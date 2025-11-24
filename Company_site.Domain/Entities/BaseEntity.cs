78c9fc4cb28f5342ca2fd9a03a1e2ac07bea9f74
Mehdi Avazniya <MehdiAvazniya@gmail.com>
2025-11-24 20:23:19 +0330

    add baseEntity && IDelete Interface

---
+++ Company_site.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_site.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ Company_site.Domain/Entities/IDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_site.Domain.Entities
+{
+    public interface IDelete
+    {
+        public bool IsDelete { get; set; }
+    }
+}
